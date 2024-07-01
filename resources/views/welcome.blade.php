9bb5d3dca567681b20ad386e3093fa0be918e2cc
Marek <marekgacek45@gmail.com>
2024-07-01 07:22:10 +0200

    install dependencies

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+
+</head>
+
+<body>
+    <h1 class="text-2xl text-red-400">Test</h1>
+</body>
+
+</html>
